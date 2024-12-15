5a7acc34cc3d40e75a3cb9fc388616c9a465b0d4
deeplearningplus <121497429+deeplearningplus@users.noreply.github.com>
2024-12-15 23:24:26 +0800

    Update spm_train_alphabet.sh

--- alphabet_tokenizer/spm_train_alphabet.sh
+++ alphabet_tokenizer/spm_train_alphabet.sh
@@ -10,15 +10,20 @@
 # How to set vocab_size?
 # 1. number of tokens in prefined_tokens.txt is 102 (n1)
 # 2. number of lines in alpha.txt is 95 (n2)
-# vocab_size = n1 + n2 + 3 = 102 + 95 + 3 = 200
+# 3. number of special tokens 4 (k, i.e., unk_id, bos_id, eos_id, pad_id)
+# vocab_size = n1 + n2 + k = 102 + 95 + 4 = 201
+
+# By default, spm_train disable pad_id (set to -1).
+# By default, unk_id=0, bos_id=1, eos_id=2
 
 spm_train \
 --input=alpha.txt \
 --add_dummy_prefix=false \
 --model_prefix=alphabet \
+--unk_id 0 --bos_id 1 --eos_id 2 --pad_id 3 \
 --input_format="text" --model_type=bpe --allow_whitespace_only_pieces=true --split_digits=true \
 --unk_surface=" \342\201\207 " --normalization_rule_name=identity --byte_fallback=false \
---vocab_size=200 \
+--vocab_size=201 \
 --character_coverage=1.0 \
 --split_by_whitespace=true \
 --user_defined_symbols=$(cat prefined_tokens.txt | tr '\n' ',')
